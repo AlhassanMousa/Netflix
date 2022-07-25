abc0c1de1416410c085f239ec0c7a803eff3173f
Alhassan Mousa <AlhassanMousa97@gmail.com>
2022-07-25 02:57:47 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhz2SQl0fQbtWIzP3aq5S5f-4MllyVX8w",
-    authDomain: "xoshelf.firebaseapp.com",
-    projectId: "xoshelf",
-    storageBucket: "xoshelf.appspot.com",
-    messagingSenderId: "821990135557",
-    appId: "1:821990135557:web:a027051302472bd210096c",
-    measurementId: "G-VEJ6L6H7C5"
+    apiKey: : "##########",
+    authDomain: : "##########",
+    projectId: : "##########",
+    storageBucket: : "##########",
+    messagingSenderId:: "##########",
+    appId: : "##########",
+    measurementId: "##########"
 };
 
 firebase.initializeApp(firebaseConfig);
